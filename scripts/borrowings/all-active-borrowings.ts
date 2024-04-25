97fd5820f797dfebedcfba4708360f09f752ef63
Ferran Bals - iMac <ferranbals@gmail.com>
2024-04-25 16:51:38 +0200

    refactor borrowing scripts responses

--- scripts/borrowings/all-active-borrowings.ts
+++ scripts/borrowings/all-active-borrowings.ts
@@ -1,7 +1,7 @@
 import { findActiveBorrowings } from "../../src/borrowings";
 
 const borrowings = await findActiveBorrowings();
-for (const { borrowId, affiliateId, bookId } of borrowings) {
-  console.log(`${borrowId} borrowing, by affiliate ${affiliateId}. Book: ${bookId}`);
+for (const { borrowId, bookId } of borrowings) {
+  console.log(`Borrow id ${borrowId}, by affiliate ${affiliateId}. Book: ${bookId}`);
 }
 console.log("Total active borrowings: ", borrowings.length);
--- scripts/borrowings/find-all-borrowings-by-affiliate-id.ts
+++ scripts/borrowings/find-all-borrowings-by-affiliate-id.ts
@@ -7,5 +7,18 @@ if (process.argv.length != 3) {
 
 const [_bun, _script, identifier] = process.argv;
 
-const result = await findBorrowingsByAffiliateId(Number(identifier));
-console.log(result);
+const borrowings = await findBorrowingsByAffiliateId(Number(identifier));
+
+console.log("Requested active borrowings of affiliate with id:", identifier);
+if (borrowings != null) {
+  console.log("Borrowings found: ", borrowings.length);
+
+  for (const { bookId, active } of borrowings) {
+    console.log(`book with id: ${bookId} was borrowed by affiliate with id: ${identifier}. `);
+    if (active) {
+      console.log(`This borrowing is still active.`);
+    } else {
+      console.log(`This borrowing is not active.`);
+    }
+  }
+}
